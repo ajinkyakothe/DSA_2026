c7da71ce53662e31f3e08b3d5f2a060c08ff70ed
Ajinkya Kothe <115250148+ajinkyakothe@users.noreply.github.com>
2026-05-25 08:22:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (39.51%) - LeetSync

---
+++ May/1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean check(int[] nums) {
+
+        //count inversion points
+        int count = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                count++;
+            }
+        }
+
+        if (nums[nums.length - 1] > nums[0]) {
+            count++;
+        }
+
+        return count <= 1;
+
+    }
+}
